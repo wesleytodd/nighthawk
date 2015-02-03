3d716ae43621b682d7fb12ea9aa69e8b5857af29
Wes Todd <wes@wesleytodd.com>
2015-02-03 09:08:21 -0600

    Housekeeping

--- router.js
+++ router.js
@@ -1,22 +1,60 @@
+/**
+ * @file Router.js
+ * @author Wes Todd
+ * @version 1.0
+ * @module netgear
+ */
+
+// Requirements
 var BaseRouter = require('router'),
 	util = require('util'),
 	supported = require('./supports-push-state');
 
+/**
+ * Router
+ *
+ * @constructor Router
+ * @memberof module:netgear
+ * @augments module:router.Router
+ * @param {Object} [options]
+ * @param {String} [options.base] - The base path for this router to match against
+ */
 var Router = module.exports = function Router(options) {
 	if (!(this instanceof Router)) {
-		return new Router(options)
+		return new Router(options);
 	}
 
-	this._base = null;
+	// Set the base path
+	this.base(options.base || null);
 
-	return BaseRouter.call(this, options);
+	// Call parent constructor
+	BaseRouter.call(this, options);
 };
 util.inherits(Router, BaseRouter);
 
+/**
+ * Set the base path for this router
+ *
+ * @function base
+ * @memberof module:netgear.Router
+ * @instance
+ * @param {String} path - The new base path
+ */
 Router.prototype.base = function(path) {
 	this._base = path;
 };
 
+/**
+ * Start listening for route chagnes
+ *
+ * @function listen
+ * @memberof module:netgear.Router
+ * @instance
+ * @param {Object} [options]
+ * @param {Boolean} [options.popstate] - Should we bind to the popstate event?
+ * @param {Boolean} [options.interceptClicks] - Should we bind to the window's click event?
+ * @param {Boolean} [options.dispatch] - Should we dispatch a route right away?
+ */
 Router.prototype.listen = function(options) {
 	// Default options
 	options = options || {};
@@ -37,11 +75,27 @@ Router.prototype.listen = function(options) {
 	}
 };
 
+/**
+ * Handler for the popstate event
+ *
+ * @function onPopstate
+ * @memberof module:netgear.Router
+ * @instance
+ * @param {Event} e
+ */
 Router.prototype.onPopstate = function(e) {
 	// Process the request
 	this._processRequest((e.state && e.state.path) || location.pathname + location.search + location.hash, true);
 };
 
+/**
+ * Handler for all click events
+ *
+ * @function onClick
+ * @memberof module:netgear.Router
+ * @instance
+ * @param {Event} e
+ */
 Router.prototype.onClick = function(e) {
 	// Cross browser event
 	e = e || window.event;
@@ -55,33 +109,33 @@ Router.prototype.onClick = function(e) {
 	var el = isLink(e.target);
 
 	// Not a link
-    if (!el) {
+	if (!el) {
 		return;
 	}
 
 	// Ignore if tag has
-    // 1. "download" attribute
-    // 2. rel="external" attribute
-    // 3. target attribute
-    if (el.getAttribute('download') || el.getAttribute('rel') === 'external' || el.target) {
+	// 1. "download" attribute
+	// 2. rel="external" attribute
+	// 3. target attribute
+	if (el.getAttribute('download') || el.getAttribute('rel') === 'external' || el.target) {
 		return;
 	}
 
 	// Get the link href
-    var link = el.getAttribute('href');
+	var link = el.getAttribute('href');
 
-    // ensure this is not a hash for the same path
-    if (el.pathname === location.pathname && (el.hash || link === '#')) {
+	// ensure this is not a hash for the same path
+	if (el.pathname === location.pathname && (el.hash || link === '#')) {
 		return;
 	}
 
-    // Check for mailto: in the href
-    if (link && link.indexOf('mailto:') > -1) {
+	// Check for mailto: in the href
+	if (link && link.indexOf('mailto:') > -1) {
 		return;
 	}
 
-    // Only for same origin
-    if (!sameOrigin(link)) {
+	// Only for same origin
+	if (!sameOrigin(link)) {
 		return;
 	}
 
@@ -94,12 +148,22 @@ Router.prototype.onClick = function(e) {
 	}
 
 	// We are all good to parse the route
-    e.preventDefault();
+	e.preventDefault();
 
 	// Run the route matching
 	this._processRequest(el.pathname);
 };
 
+/**
+ * Process a url
+ *
+ * @function onClick
+ * @memberof module:netgear.Router
+ * @instance
+ * @private
+ * @param {String} url - The new url for the page
+ * @param {Boolean} replace - Should this replace or push?
+ */
 Router.prototype._processRequest = function(url, replace) {
 	// Push the state
 	history[replace ? 'replaceState' : 'pushState']({path: url}, null, url);
@@ -115,10 +179,10 @@ Router.prototype._processRequest = function(url, replace) {
 
 // Is this element a link?
 function isLink(el) {
-    while (el && 'A' !== el.nodeName) {
+	while (el && 'A' !== el.nodeName) {
 		el = el.parentNode;
 	}
-    if (!el || 'A' !== el.nodeName) {
+	if (!el || 'A' !== el.nodeName) {
 		return;
 	}
 	return el;
