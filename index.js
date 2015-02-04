ebd1ad15b5e50ef398f99848eafd72ea4bded1ef
Wes Todd <wes@wesleytodd.com>
2015-02-03 18:32:24 -0600

    Expose the request and response objects

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Expose the Router constructor
 module.exports = require('./lib/router');
 
+// Expose request and response
+module.exports.Request = require('./lib/request');
+module.exports.Response = require('./lib/response');
+
 // Is the History API supported?
 module.exports.pushStateSupported = require('./lib/supports-push-state');
