504f4257877d87310732f4495dc18d74dfa81d36
Wes Todd <wes@wesleytodd.com>
2016-01-24 14:29:37 -0600

    updated react example to use new middleware

--- example/react/routes.js
+++ example/react/routes.js
@@ -1,30 +1,9 @@
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var ReactDOM = require('react-dom');
+var reactExpressMiddleware = require('react-express-middleware');
 
 module.exports = function routes (router) {
 	// Isomorphic react render middleware
-	router.use(function (options) {
-		options = options || {};
-		options.template = options.template || 'index';
-		options.key = options.key || 'content';
-
-		return function (req, res, next) {
-			res.renderReactComponent = function renderReactComponent (Component, store) {
-				if (typeof window === 'undefined') {
-					var tmplData = {};
-					tmplData[options.key] = ReactDOMServer.renderToString(React.createFactory(Component)(...store));
-					res.render(options.template, tmplData);
-				} else {
-					options.element = options.element || window.document.getElementById(options.elementId);
-					ReactDOM.render(React.createFactory(Component)(...store), options.element);
-				}
-			};
-			next();
-		};
-	}({
-		// options
-		elementId: 'app'
+	router.use(reactExpressMiddleware({
+		element: 'app'
 	}));
 
 	router.get('/', require('./handlers/index.jsx'));
