f3ba70c8def3f2930edfb6aa7788ca0c3768f098
Wes <wes@wesleytodd.com>
2017-03-26 22:43:17 -0500

    added mocks for all the other request properties and methods

--- test/request.js
+++ test/request.js
@@ -6,5 +6,10 @@ describe('Request', function () {
 	it('should have the correct protocol', function () {
 		var r = new Request();
 		assert.equal(r.protocol, 'http');
+		assert.equal(r.secure, false);
+	});
+	it('should have the correct hostname', function () {
+		var r = new Request();
+		assert.equal(r.hostname, window.location.hostname);
 	});
 });
